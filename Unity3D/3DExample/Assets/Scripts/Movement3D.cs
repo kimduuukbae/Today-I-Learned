382db413ff6aabecd34d7e8eb937b9dd10b85b3f
kimduckbae <qlccksdlf@gmail.com>
2021-01-26 02:49:59 +0900

    Add CS, .gitignore

---
+++ Unity3D/3DExample/Assets/Scripts/Movement3D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Movement3D : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+    private float gravity = -9.81f;
+    private Vector3 moveDirection;
+    
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (!controller.isGrounded)
+            moveDirection.y += gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetDirection(Vector3 dir)
+    {
+        moveDirection = new Vector3(dir.x, moveDirection.y, dir.z);
+    }
+}
---
+++ Unity3D/3DExample/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Movement3D movement3D;
+    private const string h = "Horizontal";
+    private const string v = "Vertical";
+
+    private void Awake()
+    {
+        movement3D = GetComponent<Movement3D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw(h);
+        float z = Input.GetAxisRaw(v);
+
+        movement3D.SetDirection(new Vector3(x, 1, z));
+    }
+}
