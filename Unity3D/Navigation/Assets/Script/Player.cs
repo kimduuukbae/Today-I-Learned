6b754f2a7d0b9cf19ebc7c554330000c10470444
kimduckbae <qlccksdlf@gmail.com>
2021-01-31 03:33:37 +0900

    Add MouseInput

--- Unity3D/Navigation/Assets/Script/Player.cs
+++ Unity3D/Navigation/Assets/Script/Player.cs
@@ -4,18 +4,25 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     private NavMeshAgent _agent;
-
-    [SerializeField] 
-    private Transform target;
+    private Camera cam;
+    [SerializeField] private LayerMask mask;
 
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main; // Tag∞° MainCamera¿Œ GameObject
     }
 
     private void Update()
     {
-        _agent.SetDestination(target.position);
+        if(Input.GetMouseButtonDown(0))
+        {
+            var mousePosition = Input.mousePosition;
+            var ray = cam.ScreenPointToRay(mousePosition);
+
+            if (Physics.Raycast(ray, out var hitInfo, mask))
+                _agent.SetDestination(hitInfo.point);
+        }
     }
 }
 
