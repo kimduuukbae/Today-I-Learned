0cf917ffedebc896ff45116650ddd9585a0a778a
kimduckbae <qlccksdlf@gmail.com>
2021-02-02 01:24:23 +0900

    Add 2D Animation

---
+++ Unity3D/2DExample/Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetTrigger("isDie");
+    }
+}
